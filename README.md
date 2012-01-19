6de7422d232f8d68403d530ad282b29c29623f52
Allan Boyd <millionyearsold@gmail.com>
2012-01-19 20:44:15 +0000

    Implementation, just 3 common exceptions for now, tests, README.md and package ready to go.

--- README.md
+++ README.md
@@ -1,5 +1,56 @@
 Exceptions
 ==========
 
-TDB
+Trivial API and associated static instances of Exception that make it slightly simpler to throw common and
+custom exceptions that are not already available in Javascript. Using static instances for Exceptions also
+helps to maintain consistency between exception names both when throwing and catching (and reduces the likelihood
+that typos in name literal strings cause bugs).
+
+The idea is that this works with the current Javascript Error mechanism as a complement to it. Exceptions and Errors
+are meant to co-exist - in fact all Exceptions really do is throw Errors and provide a well defined means to compare
+Errors within a catch block (for example).
+
+Hopefully the list of static common Exception instances will grow over time.
+
+Installation
+------------
+
+    $npm install exceptions
+
+
+Alternatively include as a dependency within your *package.json* and:
+
+    $npm link
+
+
+Usage
+-----
+
+    var exceptions = require("exceptions");
+
+
+Then invoke the provided APIs on instances of Error or Error.stack as required.
+
+
+Examples
+--------
+
+See test/testExceptions.js for some examples.
+
+
+Testing
+-------
+
+Tests utilise [nodeunit](https://github.com/caolan/nodeunit). In addition jshint is run against both lib and test
+javascript files.
+
+First install the dependencies:
+
+    $ npm link
+
+Then to run the tests:
+
+    $ npm test
+
+
 
--- test/testExceptions.js
+++ test/testExceptions.js
@@ -2,11 +2,46 @@ var exceptions = require("../lib/exceptions");
 var should = require("should");
 
 exports.testIllegalArgumentException = function (test) {
-
     try {
-        exceptions.illegalArgumentException("NOOOOO");
+        exceptions.ILLEGAL_ARGUMENT.thro("NOOOO");
     } catch (error) {
-        console.log(error.stack);
+        error.name.should.equal("IllegalArgumentException");
+        error.stack.should.not.include("exceptions.js");
+        error.stack.should.include("NOOOO");
+        test.done();
     }
-    test.done();
+};
+
+exports.testIllegalStateException = function (test) {
+    try {
+        exceptions.ILLEGAL_STATE.thro("Bad state");
+    } catch (error) {
+        error.name.should.equal("IllegalStateException");
+        error.stack.should.not.include("exceptions.js");
+        error.stack.should.include("Bad state");
+        test.done();
+    }
+};
+
+exports.testIOException = function (test) {
+    try {
+        exceptions.IO.thro("Bad IO");
+    } catch (error) {
+        error.name.should.equal("IOException");
+        error.stack.should.not.include("exceptions.js");
+        error.stack.should.include("Bad IO");
+        test.done();
+    }
+};
+
+exports.testCustomException = function (test) {
+    var NOT_ENCAPSULATED_EXCEPTION = new exceptions.Exception("NotEncapsulatedException");
+    try {
+        NOT_ENCAPSULATED_EXCEPTION.thro("Exception does not protect name and thro members");
+    } catch (error) {
+        error.name.should.equal("NotEncapsulatedException");
+        error.stack.should.not.include("exceptions.js");
+        error.stack.should.include("Exception does not protect name and thro members");
+        test.done();
+    }
 };
